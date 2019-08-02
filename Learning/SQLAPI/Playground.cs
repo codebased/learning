8b8c6536f82f9cd88099216fe69bdc0c1d02f859
Amit Malhotra <codebased@hotmail.com>
2019-08-02 12:07:42 +1000

    using cosmodb tableapi and sql api

---
+++ Learning/SQLAPI/Playground.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Learning.SQLAPI
+{
+    public class Playground : Controller
+    {
+        public Playground(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("/sql")]
+        [HttpGet]
+        public async Task<ActionResult<List<Family>>> Init()
+        {
+            return await GetStartedDemoAsync();
+        }
+
+        private CosmosClient cosmosClient;
+
+        private Database database;
+
+        private Container container;
+
+        private string databaseId = "FamilyDatabase";
+        private string containerId = "FamilyContainer";
+
+        private IConfiguration _configuration;
+
+        private async Task CreateDatabaseAsync()
+        {
+            database = await cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
+            Console.WriteLine("Created Database: {0}\n", database.Id);
+        }
+
+        private async Task CreateContainerAsync()
+        {
+            container = await database.CreateContainerIfNotExistsAsync(containerId, "/LastName");
+            Console.WriteLine("Created Container: {0}\n", container.Id);
+        }
+
+        private async Task AddItemsToContainerAsync()
+        {
+            Family andersenFamily = new Family
+            {
+                Id = "Andersen.1",
+                LastName = "Andersen",
+                Parents = new Parent[]
+                {
+           new Parent { FirstName = "Thomas" },
+           new Parent { FirstName = "Mary Kay" }
+                },
+                Children = new Child[]
+                {
+           new Child
+            {
+                FirstName = "Henriette Thaulow",
+                Gender = "female",
+                Grade = 5,
+                Pets = new Pet[]
+                {
+                    new Pet { GivenName = "Fluffy" }
+                }
+            }
+                },
+                Address = new Address { State = "WA", County = "King", City = "Seattle" },
+                IsRegistered = false
+            };
+
+            try
+            {
+                ItemResponse<Family> andersenFamilyResponse = await container.ReadItemAsync<Family>(andersenFamily.Id, new PartitionKey(andersenFamily.LastName));
+                Console.WriteLine("Item in database with id: {0} already exists\n", andersenFamilyResponse.Resource.Id);
+            }
+            catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+            {
+                ItemResponse<Family> andersenFamilyResponse = await container.CreateItemAsync<Family>(andersenFamily, new PartitionKey(andersenFamily.LastName));
+                Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", andersenFamilyResponse.Resource.Id, andersenFamilyResponse.RequestCharge);
+            }
+        }
+
+        private async Task<List<Family>> QueryItemsAsync()
+        {
+            var sqlQueryText = "SELECT * FROM c WHERE c.LastName = 'Andersen'";
+
+            Console.WriteLine("Running query: {0}\n", sqlQueryText);
+
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
+            FeedIterator<Family> queryResultSetIterator = container.GetItemQueryIterator<Family>(queryDefinition);
+
+            List<Family> families = new List<Family>();
+
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<Family> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (Family family in currentResultSet)
+                {
+                    families.Add(family);
+                    Console.WriteLine("\tRead {0}\n", family);
+                }
+            }
+
+            return families;
+        }
+
+        private async Task DeleteDatabaseAndCleanupAsync()
+        {
+            DatabaseResponse databaseResourceResponse = await database.DeleteAsync();
+            cosmosClient.Dispose();
+        }
+
+        private async Task<List<Family>> GetStartedDemoAsync()
+        {
+            // Create a new instance of the Cosmos Client
+            cosmosClient = new CosmosClient(_configuration["CosmoDbStorageSQLAPConnectionString"]);
+            await CreateDatabaseAsync();
+            await CreateContainerAsync();
+            await AddItemsToContainerAsync();
+            return await QueryItemsAsync();
+        }
+    }
+}
---
+++ Learning/TableAPI/Playground.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace Learning.TableAPI
+{
+    public class Playground : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public Playground(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+
+        [Route("/tables")]
+        [HttpGet]
+        public async Task<ActionResult<CustomerEntity>> Start()
+        {
+            var table = await CreateTableAsync("customers");
+            await InsertOrMergeEntityAsync(table, new CustomerEntity("malhotra", "amit") { Email = "codebased@hotmail.com", PhoneNumber = "9239472" });
+            return await RetrieveEntityUsingPointQueryAsync(table, "malhotra", "amit");
+        }
+
+        private async Task<CustomerEntity> InsertOrMergeEntityAsync(CloudTable table, CustomerEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            // Create the InsertOrReplace table operation
+            TableOperation insertOrMergeOperation = TableOperation.InsertOrMerge(entity);
+
+            // Execute the operation.
+            TableResult result = await table.ExecuteAsync(insertOrMergeOperation);
+            CustomerEntity insertedCustomer = result.Result as CustomerEntity;
+
+            return insertedCustomer;
+        }
+
+        private async Task<CustomerEntity> RetrieveEntityUsingPointQueryAsync(CloudTable table, string partitionKey, string rowKey)
+        {
+            TableOperation retrieveOperation = TableOperation.Retrieve<CustomerEntity>(partitionKey, rowKey);
+            TableResult result = await table.ExecuteAsync(retrieveOperation);
+            CustomerEntity customer = result.Result as CustomerEntity;
+
+            return customer;
+        }
+
+        private async Task<CustomerEntity> DeleteEntityAsync(CloudTable table, CustomerEntity deleteEntity)
+        {
+            if (deleteEntity == null)
+            {
+                throw new ArgumentNullException("deleteEntity");
+            }
+
+            TableOperation deleteOperation = TableOperation.Delete(deleteEntity);
+            TableResult result = await table.ExecuteAsync(deleteOperation);
+            return result.Result as CustomerEntity;
+
+        }
+
+        private async Task QueryAsync(CloudTable table, string lastName, string email)
+        {
+            TableQuery<CustomerEntity> query = new TableQuery<CustomerEntity>()
+                .Where(
+                    TableQuery.CombineFilters(
+                        TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, lastName),
+                        TableOperators.And,
+                        TableQuery.GenerateFilterCondition("Email", QueryComparisons.Equal, email)
+                ));
+
+            await table.ExecuteQuerySegmentedAsync(query, null);
+        }
+
+        private async Task<CloudTable> CreateTableAsync(string tableName)
+        {
+            string storageConnectionString = _configuration["StorageConnectionString"];
+
+            // Retrieve storage account information from connection string.
+            var storageAccount = Common.CreateStorageAccountFromConnectionString(storageConnectionString);
+
+            // Create a table client for interacting with the table service
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            Console.WriteLine("Create a Table for the demo");
+
+            // Create a table client for interacting with the table service 
+            CloudTable table = tableClient.GetTableReference(tableName);
+            if (await table.CreateIfNotExistsAsync())
+            {
+                Console.WriteLine("Created Table named: {0}", tableName);
+            }
+            else
+            {
+                Console.WriteLine("Table {0} already exists", tableName);
+            }
+
+            Console.WriteLine();
+            return table;
+        }
+    }
+}
