0de3a71d7079a0c44cacfc68377d893ba9890fa9
Amit Malhotra <codebased@hotmail.com>
2019-08-20 22:31:05 +1000

    Moving BoyerMooreHorspoolSearch & Impl DFS.

---
+++ Learning/Search/DepthFirstSearch/DFS.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learning.Search.DepthFirstSearch
+{
+    public class DFS
+    {
+        public List<T> VisitNodes<T>(Graph<T> graph, T start )  
+        {
+            var visitedNodes = new List<T>();
+ 
+            if (!graph.NodeList.ContainsKey(start))
+            {
+                return visitedNodes;
+            }
+
+            var stack = new ConcurrentStack<T>();
+            stack.Push(start);
+            while (stack.Count > 0)
+            {
+                if (stack.TryPop(out T node) && !visitedNodes.Contains(node))
+                {
+                    visitedNodes.Add(node);
+                    foreach (var childNodes in graph.NodeList[node])
+                    {
+                        if (!visitedNodes.Contains(childNodes))
+                        {
+                            stack.Push(childNodes);
+                        }
+                    }
+                }
+            }
+
+            return visitedNodes;
+
+        }
+    }
+
+    public class Graph<T>
+    {
+        private readonly ConcurrentDictionary<T, HashSet<T>> _graph;
+
+        public Graph()
+        {
+            _graph = new ConcurrentDictionary<T, HashSet<T>>();
+        }
+
+        public ConcurrentDictionary<T, HashSet<T>> NodeList
+        {
+            get
+            {
+                return _graph;
+            }
+        }
+        public Graph(IEnumerable<T> nodes, IEnumerable<Tuple<T, T>> edges) : this()
+        {
+
+            foreach (var item in nodes)
+            {
+                // adding all nodes/vertex
+                _graph.TryAdd(item, new HashSet<T>());
+            }
+
+            foreach (var edge in edges)
+            {
+                if (_graph.ContainsKey(edge.Item1) && _graph.ContainsKey(edge.Item2))
+                {
+                    _graph[edge.Item1].Add(edge.Item2);
+                    _graph[edge.Item2].Add(edge.Item1);
+                }
+            }
+        }
+
+    }
+}
---
+++ LearningTests/DFSTests.cs
@@ -0,0 +1,65 @@
+﻿using Learning.Search.DepthFirstSearch;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace LearningTests
+{
+    [TestClass]
+    public class DFSTests
+    {
+        public class Node
+        {
+            public int Info { get; set; }
+            public Node()
+            {
+
+
+            }
+
+            public override string ToString()
+            {
+                return Info.ToString();
+            }
+        }
+
+        [TestMethod]
+        public void DFS_Traverse()
+        {
+
+            var dfs = new DFS();
+
+            var nodeList = new List<Node>();
+
+            nodeList.Add(new Node { Info = 1 });
+            nodeList.Add(new Node { Info = 2 });
+            nodeList.Add(new Node { Info = 3 });
+
+            var edges = new List<Tuple<Node, Node>>();
+            edges.Add(new Tuple<Node, Node>(nodeList[0], nodeList[1]));
+            edges.Add(new Tuple<Node, Node>(nodeList[0], nodeList[2]));
+
+            var visitedNodes = dfs.VisitNodes(new Graph<Node>(nodeList, edges), nodeList[0]);
+            foreach (var node in visitedNodes)
+            {
+                Console.WriteLine(node.ToString());
+            }
+        }
+
+        [TestMethod]
+        public void DFS_()
+        {
+            var vertices = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var edges = new[]{Tuple.Create(1,2), Tuple.Create(1,3),
+                Tuple.Create(2,4), Tuple.Create(3,5), Tuple.Create(3,6),
+                Tuple.Create(4,7), Tuple.Create(5,7), Tuple.Create(5,8),
+                Tuple.Create(5,6), Tuple.Create(8,9), Tuple.Create(9,10)};
+
+            var graph = new Graph<int>(vertices, edges);
+            var dfs = new DFS();
+            // Dive Deep: 1 -> 3 -> 6 -> 5 -> 8 -> 9 -> 10, and then backtrack to 5 and dive deep again: 7 -> 4-> 2.
+            Console.WriteLine(string.Join(", ", dfs.VisitNodes(graph, 1)));
+        }
+
+    }
+}
