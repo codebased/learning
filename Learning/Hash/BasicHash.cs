229fd50cee5d662d34c70a6691ad1656466108a1
Amit Malhotra <codebased@hotmail.com>
2019-08-26 23:03:10 +1000

    adding basic hash function implementiation with chaining

---
+++ Learning/Hash/BasicHash.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Learning.Hash
+{
+    public class BasicHash
+    {
+        private readonly List<List<int>> _bucket;
+
+        public BasicHash(int bucketLength)
+        {
+            _bucket = new List<List<int>>(bucketLength);
+        }
+
+        public void Add(int value)
+        {
+            var chain = _bucket[HashFunction(value)];
+
+            // Don't want to add anything that is already added in the chain.
+            if (!chain.Contains(value))
+            {
+                chain.Add(value);
+            }
+        }
+
+        public void Remove(int value)
+        {
+            var chain = _bucket[HashFunction(value)];
+            if (chain.Contains(value))
+            {
+                chain.Remove(value);
+            }
+        }
+
+        private int HashFunction(int value)
+        {
+            return value % _bucket.Count;
+        }
+    }
+}
--- Learning/Search/BreadthFirstSearch/BFS.cs
+++ Learning/Search/BreadthFirstSearch/BFS.cs
@@ -9,6 +9,26 @@ namespace Learning.Search.BreadthFirstSearch
 
     public class BFS
     {
+        // It is Graph traversal strategy 
+        /*
+         * The major difference between BFS and DFS is that BFS proceeds level by level 
+         * while DFS follows first a path form the starting to the ending node (vertex), 
+         * then another path from the start to end, and so on until all nodes are visited. 
+         * Furthermore, BFS uses the queue for storing the nodes whereas DFS uses the stack for traversal of the nodes.
+         * Key Differences Between BFS and DFS
+
+         *  BFS is vertex-based algorithm while DFS is an edge-based algorithm.
+         *   Queue data structure is used in BFS. On the other hand, DFS uses stack or recursion.
+         *   Memory space is efficiently utilized in DFS while space utilization in BFS is not effective.
+         *   BFS is optimal algorithm while DFS is not optimal.
+         *   DFS constructs narrow and long trees. As against, BFS constructs wide and short tree.
+
+         *   REF: https://techdifferences.com/difference-between-bfs-and-dfs.html
+         *   
+         *   Usage: 
+         *   BFS is also used in the famous Dijkstra’s algorithm for computing the shortest path in a graph and the Ford-Fulkerson 
+         *   algorithm for computing the maximum​ flow in a flow network.
+         * */
         public List<T> Traverse<T>(Graph<T> graph, T start)
         {
             var visitedNodes = new List<T>();
